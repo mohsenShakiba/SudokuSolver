55f9675af450f75a30ec6cb6f137df0d399e9aa0
user <work.shakiba@gmail.comm>
2020-01-13 01:44:28 +0330

    added permutation and validation for test

--- Sudoku/RandomInputGenerator.cs
+++ Sudoku/RandomInputGenerator.cs
@@ -1,19 +1,53 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sudoku
 {
     public class RandomInputGenerator
     {
-        private readonly List<Input> Inputs;
 
-        public RandomInputGenerator()
+        public static void Permutation(Chart chart, Square square, int minCount)
         {
-            Inputs = new List<Input>();
-            for (int i = 0; i < 3; i++)
+            var rnd = new Random(1000);
+            while (square.Count() < minCount)
             {
-                for (int j = 0; j < 3; j++)
+                var row  = rnd.Next(1, square.Size + 1);
+                var column  = rnd.Next(1, square.Size + 1);
+                var input = square.GetInput(row, column);
+                if (input.Value != null)
+                    continue;
+                var value  = rnd.Next(1, square.Size * square.Size);
+                input.Value = value;
+                if (!chart.IsInputValidForSquare(square, input))
                 {
-                    Inputs.Add(new Input());
+                    input.Value = null;
+                }
+            }
+        }
+
+        public static void RemoveViolatingRows(Chart chart)
+        {
+            foreach (var square in chart.Squares)
+            {
+                var neighboringRows = chart.NeighbourRowSquares(square);
+                var neighboringColumns = chart.NeighbourColumnSquares(square);
+                var distinctValues = new List<int>();
+                var distinctRowValues = neighboringRows.SelectMany(s => s.Inputs).Where(i => i.Value.HasValue).Select(i => i.Value.Value).Distinct();
+                var distinctColumnValues = neighboringColumns.SelectMany(s => s.Inputs).Where(i => i.Value.HasValue).Select(i => i.Value.Value).Distinct();
+                distinctValues.AddRange(distinctRowValues);
+                distinctValues.AddRange(distinctColumnValues);
+                distinctValues = distinctValues.Distinct().ToList(); 
+                
+                foreach (var distinctValue in distinctValues)
+                {
+                    var matchingRows = neighboringRows.SelectMany(s => s.Inputs).Where(i => i.Value == distinctValue).Select(i => i.Row);
+                    var matchingColumns = neighboringColumns.SelectMany(s => s.Inputs).Where(i => i.Value == distinctValue).Select(i => i.Column);
+                    var results = square.Inputs
+                        .Where(i => !matchingRows.Contains(i.Row))
+                        .Where(i => !matchingColumns.Contains(i.Column));
+                    if (!results.Any(i => i.Value == null))
+                        results.First().Value = null;
                 }
             }
         }
