f75fcab7014e09758eefc51f34b9b9a754fea602
m.shakiba <work.shakiba@gmail.com>
2020-01-15 14:39:56 +0330

    changed string builder

--- Sudoku/Presentation/StringPresentation.cs
+++ Sudoku/Presentation/StringPresentation.cs
@@ -10,55 +10,25 @@ namespace Sudoku.Presentation
     {
         public void Present(Chart chart)
         {
-            string PrintRowSeperator()
-            {
-                var sb = new StringBuilder();
-                var totalColumnsInRepresentationStr = (chart.Class * chart.Class) + (chart.Class - 1) + 2;
-                
-                for (int j = 0; j < totalColumnsInRepresentationStr; j++)
-                {
-                    if (j % (chart.Class + 1) == 0)
-                        sb.Append("   ");
-                    else
-                        sb.Append("---");
-                }
-                
-                sb.AppendLine();
-
-                return sb.ToString();
-            }
-            
             var sb = new StringBuilder();
 
-            sb.Append(PrintRowSeperator());
-            
-            for (int i = 0; i < chart.Class * chart.Class; i++)
+            foreach (var input in chart.Inputs)
             {
-                var boxRow = (i / 3) + 1;
-                var inputRow = (i % 3) + 1;
-                var columnRange = 0..chart.Class;
-                var boxes = chart.Boxes.Where(s =>
-                    s.Row == boxRow);
-                var inputs = boxes.SelectMany(i => i.Inputs).Where(i =>
-                    i.Row == inputRow &&
-                    i.Column >= columnRange.Start.Value && 
-                    i.Column <= columnRange.End.Value);
+                if (input.Row == 1)
+                    sb.Append(" | ");
                 
-                sb.Append(" | ");
+//                if (input.Column == 1)
+//                    sb.Append("---");
                 
-                foreach (var input in inputs)
-                {
-                    sb.Append($" {input.Value?.ToString() ?? " "} ");
-                    if (input.Column == chart.Class)
-                        sb.Append(" | ");
-                }
-
-                sb.AppendLine();
-
-                if (inputRow == 3)
-                    sb.Append(PrintRowSeperator());
+                sb.Append($" {input.Value?.ToString() ?? "0"} ");
+                
+                if (input.Row == 3)
+                    sb.AppendLine(" | ");
+                
+//                if (input.Column == 1)
+//                    sb.Append("---");
             }
-
+            
             Console.WriteLine(sb.ToString());
         }
     }
