62449b2e5908dec40d7c26315e5bba5c822f903c
user <work.shakiba@gmail.comm>
2020-01-22 00:01:06 +0330

    removed unused files

--- Sudoku/Generation/ConflictResolvers/IResolver.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sudoku.Models;
-
-namespace Sudoku.ConflictResolvers
-{
-    public interface IResolver
-    {
-        void ResolveConflict(Chart chart, Input input);
-    }
-}
--- Sudoku/Validators/IValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sudoku.Models;
-
-namespace Sudoku.Validators
-{
-    public interface IValidator
-    {
-        bool IsValid(Chart chart, Input input);
-    }
-}
